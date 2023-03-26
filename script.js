019080d9938cd7524676fbb4875fa2e0f9c93c78
Uyen-Nhu Tran <125201939+uyen-nhu@users.noreply.github.com>
2023-03-26 18:42:17 +0200

    Added function to toggle class "eliminated" on card elements
    Added onclick attribute on card elements in index.html and created function "toggleEliminated" in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Guess Who?')
+// console.log('Guess Who?')
+
+let card = document.querySelector('.card')
+// console.log(card)
+
+function toggleEliminated(el) {
+    el.classList.toggle('eliminated')
+    // console.log(el)
+}
